bee3854fc8b303cf17469df859fc1af7521670c7
afomachinenyeonyekaba <afomachukwuchinenyeonyekaba@gmail.com>
2022-02-03 03:36:15 -0800

    project1

---
+++ Project1.py
@@ -0,0 +1,9 @@
+#Program to create a Band Name Generator
+
+print("Welcome to the Band Name Generator.")
+
+city = input("What's name of the city you grew up in?\n")
+
+pet = input("What's your pet's name?\n")
+
+print("Your band name could be " + city + " " + pet)
