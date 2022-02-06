df66b1b533f6cf29b519ea157a09e827694f709a
afomachinenyeonyekaba <afomachukwuchinenyeonyekaba@gmail.com>
2022-02-06 11:18:52 -0800

    effectedcorrections

--- subsectionsofproject4Whowillpaythebill.py
+++ subsectionsofproject4Whowillpaythebill.py
@@ -12,5 +12,5 @@ random_choice = random.randint(0, num_items-1)
 
 The_man_who_will_pay_the_bill= names[random_choice]
 
-print (The_man_who_will_pay_the_bill + "is going to buy the meal today")
+print (The_man_who_will_pay_the_bill + " " + "is going to buy the meal today")
 
