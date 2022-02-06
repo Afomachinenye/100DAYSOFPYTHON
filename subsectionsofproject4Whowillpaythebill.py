c9cec70ed47d974163240da037a70f34d554e5a0
afomachinenyeonyekaba <afomachukwuchinenyeonyekaba@gmail.com>
2022-02-06 11:19:53 -0800

    addedanewproject

--- subsectionsofproject4Whowillpaythebill.py
+++ subsectionsofproject4Whowillpaythebill.py
@@ -2,7 +2,7 @@
 
 import random
 
-names_string = input("Give me everybody's names, separated by a comma. ")
+names_string = input("Give me everybody's names, separated by a comma: ")
 
 names = names_string.split(",")
 
